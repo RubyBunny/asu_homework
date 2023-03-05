8007b027c7b9776908038c4b27befefec4254624
RubyBunny <rubybunny2501@gmail.com>
2023-03-05 15:29:58 +0700

    refactoring code

--- Information theory/1lab/file_reader/file_reader.py
+++ Information theory/1lab/file_reader/file_reader.py
@@ -5,11 +5,13 @@ class FileReader:
     def __init__(self, filename: str) -> None:
         self.__filename = filename
 
+
     def get_text_from_file(self) -> str:
         with open(self.__filename, "r", encoding="utf-8") as file:
             text = file.read()
 
         return text.lower()
+
 
     def create_file_copy(self, copy_name: str="copy.txt"):
         with open(copy_name, 'w', encoding="utf-8") as copy:
--- Information theory/1lab/frequency_analysis/frequency_analysis_formatter.py
+++ Information theory/1lab/frequency_analysis/frequency_analysis_formatter.py
@@ -1,3 +1,4 @@
-def frequency_analysis_formatter(letter_counter_list):
+from typing import List, Tuple
+def frequency_analysis_formatter(letter_counter_list: List[Tuple]):
     for letter_tuple in letter_counter_list:
         print(f"{letter_tuple[0]}: {letter_tuple[1]}")
