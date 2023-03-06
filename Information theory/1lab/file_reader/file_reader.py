a1c4791e9b81a57704b74b993d14fa0d1f0963f2
RubyBunny <rubybunny2501@gmail.com>
2023-03-06 20:24:47 +0700

    modificate FileReader module

--- Information theory/1lab/file_reader/file_reader.py
+++ Information theory/1lab/file_reader/file_reader.py
@@ -1,18 +1,20 @@
 class FileReader:
-    
-    __filename: str
 
-    def __init__(self, filename: str) -> None:
-        self.__filename = filename
-
-
-    def get_text_from_file(self) -> str:
-        with open(self.__filename, "r", encoding="utf-8") as file:
+    @staticmethod
+    def get_text_from_file(filename: str) -> str:
+        with open(filename, "r", encoding="utf-8") as file:
             text = file.read()
 
         return text.lower()
 
 
-    def create_file_copy(self, copy_name: str="copy.txt"):
+    @classmethod
+    def create_file_copy(cls, filename: str, copy_name: str="copy.txt"):
         with open(copy_name, 'w', encoding="utf-8") as copy:
-            copy.write(self.get_text_from_file())
+            copy.write(cls.get_text_from_file(filename))
+    
+
+    @staticmethod
+    def write_text_in_file(filename: str, text: str):
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(text)
