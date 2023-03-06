28af924ccaed59f852e6534f05d19a14e16942b6
RubyBunny <rubybunny2501@gmail.com>
2023-03-06 20:02:08 +0700

    refactoring code

--- Information theory/1lab/frequency_analysis/frequency_analysis_formatter.py
+++ Information theory/1lab/frequency_analysis/frequency_analysis_formatter.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
+
+
 def frequency_analysis_formatter(letter_counter_list: List[Tuple]):
     for letter_tuple in letter_counter_list:
         print(f"{letter_tuple[0]}: {letter_tuple[1]}")
