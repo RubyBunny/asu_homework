c6d920226eb7d324b959d006074d9d5a718873d2
RubyBunny <rubybunny2501@gmail.com>
2023-04-14 23:13:19 +0700

    refactoring (frequency_formatter.py): Добавлены '' у вывода символов

--- Information theory/2lab/program/frequency_formatter.py
+++ Information theory/2lab/program/frequency_formatter.py
@@ -1,3 +1,3 @@
 def frequency_analysis_formatter(letter_counter_list):
     for letter_tuple in letter_counter_list:
-        print(f"{letter_tuple[0]}: {letter_tuple[1]}")
+        print(f"'{letter_tuple[0]}': {letter_tuple[1]}")
