70ec539e339b40336d105cabf158c4f0b53aaa03
RubyBunny <rubybunny2501@gmail.com>
2023-05-15 19:28:46 +0700

    update (TMP/6lab): добавление 6 лабораторной

--- TMP/6lab/ListWrapper.py
+++ TMP/6lab/ListWrapper.py
@@ -1,12 +1,12 @@
-from typing import Any
+from uuid import uuid1
 
 
 class ListWrapper:
-    array: list
     
-    def __init__(self, array: list[Any]=[]) -> None:
-        self.array = array
+    array: list
 
+    def __init__(self, array: list=[]) -> None:
+        self.array = list(map(lambda item: (uuid1().int, item), array.copy()))
 
     def __repr__(self) -> str:
         return f"{self.array}"
@@ -14,25 +14,58 @@ class ListWrapper:
     def __str__(self) -> str:
         return f"{self.array}"
 
-
     def __len__(self) -> int:
         return len(self.array)
 
-
-    def __getitem__(self, key):
-        return self.array[key]
-
-    def __setitem__(self):
-        raise Exception("")
-
-    def __delitem__(self, key):
-        del self.array[key]
-
+    def __getitem__(self, index: int):
+        return self.array[index]
 
     def __iter__(self):
         return iter(self.array)
 
+    def __delitem__(self, index: int):
+        del self.array[index]
 
+    def __setitem__(self, index, new_value):
+        raise Exception("Can't set item")
 
-if __name__ == "__main__":
-    wrapper = ListWrapper()
+    def insert(self, item):
+        self.array.append(item)
+
+    def find(self, item_id: int) -> int:
+        low = 0
+        high = len(self.array) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            mid_val = self.array[mid][0]
+
+            if mid_val == item_id:
+                return mid
+            
+            if mid_val > item_id:
+                high = mid - 1
+            
+            else:
+                low = mid + 1
+
+        return len(self.array)
+    
+    def __contains__(self, item_id):
+        low = 0
+        high = len(self.array) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            mid_val = self.array[mid][0]
+
+            if mid_val == item_id:
+                return True
+            
+            if mid_val > item_id:
+                high = mid - 1
+            
+            else:
+                low = mid + 1
+
+        return False
---
+++ TMP/6lab/fuzzy_number.py
@@ -0,0 +1,115 @@
+class FuzzyNumber:
+    """This is FuzzyNumber class"""
+    __counter: int = 0
+    __x: float
+    __xl: float
+    __xr: float
+
+
+    def __init__(self, x: float, xl: float, xr: float):
+        self.__class__.__counter += 1
+        self.x = x
+        self.xl = xl
+        self.xr = xr
+
+    def __del__(self):
+        self.__class__.__counter -= 1
+
+
+    @property
+    def x(self):
+        # print("Get x value")
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        # print(f"Set in x new value = {value}")
+        self.__x = value
+
+    @property
+    def xl(self):
+        # print("Get xl value")
+        return self.__xl
+
+    @xl.setter
+    def xl(self, value):
+        # print(f"Set in xl new value = {value}")
+        self.__xl = value
+
+    @property
+    def xr(self):
+        # print("Get xr")
+        return self.__xr
+
+    @xr.setter
+    def xr(self, value):
+        # print(f"Set in xr new value = {value}")
+        self.__xr = value
+
+
+    def __add__(self, other):
+        """Return sum"""
+        return FuzzyNumber(
+                self.x + other.x - self.xl - other.xl, 
+                self.x + other.x, 
+                self.x + other.x + self.xr + other.xr
+            )
+
+    def __sub__(self, other):
+        """Return substraction"""
+        return FuzzyNumber(
+                self.x - other.x - self.xl - other.xl,
+                self.x - other.x, 
+                self.x - other.x + self.xr + other.xr
+            )
+
+    def __mul__(self, other):
+        """Return multiply"""
+        return FuzzyNumber(
+                self.x * other.x - other.x * self.xl - self.x * other.xl + self.xl * other.xl,
+                self.x * other.x, 
+                self.x * other.x + other.x * self.xr + self.x * other.xr + self.xr * other.xr
+            )
+    
+    def __truediv__(self, other):
+        """Return divide"""
+        if (other.x > 0):
+            return FuzzyNumber(
+                    (self.x - self.xl) / (other.x - other.xr),
+                    self.x / other.x,
+                    (self.x + self.xr) / (other.x - other.xl)
+                )
+
+    def __repr__(self) -> str:
+        return f"{self.x} {self.xl} {self.xr}"
+
+    def __str__(self) -> str:
+        return f"{self.x} {self.xl} {self.xr}"
+
+
+    def reverse(self):
+        """Return reversed number"""
+        if (self.x > 0):
+            return FuzzyNumber(
+                    1 / (self.x - self.xr),
+                    1 / self.x,
+                    1 / (self.x - self.xl)
+                )
+
+    @classmethod
+    def get_counter(cls) -> int:
+        """Return FuzzyNumber object count"""
+        return cls.__counter
+
+
+if __name__ == "__main__":
+    obj1 = FuzzyNumber(10, 4, 14)
+    obj2 = FuzzyNumber(15, 3, 25)
+
+    print(obj1 + obj2)
+    print(obj1 - obj2)
+    print(obj1 * obj2)
+    print(obj1 / obj2)
+    print(obj1.reverse())
+
+    print(FuzzyNumber.get_counter())
