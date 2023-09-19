47c412110e609e305524de086fe90fa422214aab
rubybunny <rubybunny2501@gmail.com>
2023-09-19 14:44:45 +0000

    hotfix (Cryptographic/1lab): Изменен язык шифрования на англиский

--- Cryptographic/1lab/libs/cipher.py
+++ Cryptographic/1lab/libs/cipher.py
@@ -13,8 +13,9 @@ class Cipher:
         unicodes = list(map(ord, list(message)))
 
         for index in range(len(unicodes)):
-            unicodes[index] = (unicodes[index] + shift * mode.value) % 32 + 1056
-            unicodes[index] += 32 if unicodes[index] < 1072 else 0
+            unicodes[index] = (unicodes[index] + shift *
+                               mode.value) % 26 + 78
+            unicodes[index] += 26 if unicodes[index] < 97 else 0
 
         return "".join(map(chr, unicodes))
 
@@ -28,7 +29,20 @@ class Cipher:
         for index in range(len(unicodes)):
             unicodes[index] = (
                 unicodes[index] + key_unicodes[index] * mode.value
-            ) % 32 + 1056
-            unicodes[index] += 32 if unicodes[index] < 1072 else 0
+            ) % 26 + 85
+            unicodes[index] += 12 if mode.value < 0 else 0
+            unicodes[index] += 26 if unicodes[index] < 97 else 0
+
+        return "".join(map(chr, unicodes))
+
+    @staticmethod
+    def gamming(message: str, key: str) -> str:
+        key = (key * ceil(len(message) / len(key)))[: len(message)]
+
+        unicodes = list(map(ord, list(message)))
+        key_unicodes = list(map(ord, list(key)))
+
+        for index in range(len(unicodes)):
+            unicodes[index] = unicodes[index] ^ key_unicodes[index]
 
         return "".join(map(chr, unicodes))
--- Cryptographic/1lab/main.py
+++ Cryptographic/1lab/main.py
@@ -1,24 +1,56 @@
+from argparse import ArgumentParser, BooleanOptionalAction
 from libs.cipher import Cipher, Mode
 from libs.file_reader import FileReader
 
-SHIFT = 3
-KEY = "паролб"
+
+argparse = ArgumentParser()
+
+cipher_algoritm_group = argparse.add_mutually_exclusive_group()
+cipher_algoritm_group.add_argument(
+    "-c", "--cesaer", type=bool, action=BooleanOptionalAction)
+cipher_algoritm_group.add_argument(
+    "-v", "--vigener", type=bool, action=BooleanOptionalAction)
+cipher_algoritm_group.add_argument(
+    "-g", "--gamming", type=bool, action=BooleanOptionalAction)
+
+cipher_mode_group = argparse.add_mutually_exclusive_group()
+cipher_mode_group.add_argument(
+    "-e", "--encode", type=bool, action=BooleanOptionalAction)
+cipher_mode_group.add_argument(
+    "-d", "--decode", type=bool, action=BooleanOptionalAction)
+
+argparse.add_argument("file", type=str)
+argparse.add_argument("-k", "--key", type=str, default="пароль")
+
+args = argparse.parse_args()
 
 
 def main():
-    original_text = FileReader.get_text_from_file("./input.txt")
+    original_text = FileReader.get_text_from_file(args.file)
 
-    caesar_text = Cipher.caesar(original_text, SHIFT, mode=Mode.encode)
-    vigener_text = Cipher.vigener(original_text, KEY, mode=Mode.encode)
+    if args.cesaer:
+        if args.encode:
+            caesar_text = Cipher.caesar(
+                original_text, int(args.key), mode=Mode.encode)
+            FileReader.write_text_in_file("ouput.txt", caesar_text)
+        elif args.decode:
+            caesar_text = Cipher.caesar(
+                original_text, int(args.key), mode=Mode.decode)
+            FileReader.write_text_in_file("ouput.txt", caesar_text)
 
-    FileReader.write_text_in_file("caesar.txt", caesar_text)
-    FileReader.write_text_in_file("vigener.txt", vigener_text)
+    if args.vigener:
+        if args.encode:
+            vigener_text = Cipher.vigener(
+                original_text, args.key, mode=Mode.encode)
+            FileReader.write_text_in_file("ouput.txt", vigener_text)
+        elif args.decode:
+            vigener_text = Cipher.vigener(
+                original_text, args.key, mode=Mode.decode)
+            FileReader.write_text_in_file("ouput.txt", vigener_text)
 
-    caesar_text = Cipher.caesar(caesar_text, SHIFT, mode=Mode.decode)
-    vigener_text = Cipher.vigener(vigener_text, KEY, mode=Mode.decode)
-
-    FileReader.write_text_in_file("caesar_orig.txt", caesar_text)
-    FileReader.write_text_in_file("vigener_orig.txt", vigener_text)
+    if args.gamming:
+        gamming_text = Cipher.gamming(original_text, args.key)
+        FileReader.write_text_in_file("ouput.txt", gamming_text)
 
 
 if __name__ == "__main__":
