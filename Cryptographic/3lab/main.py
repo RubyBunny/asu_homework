12f3088ca04f5dc391b6ce797322d4ccd6d6161c
rubybunny <rubybunny2501@gmail.com>
2023-09-22 06:44:16 +0000

    refactoring (Cryptographic/3lab): Оптимизирован алгоритм

--- Cryptographic/3lab/main.py
+++ Cryptographic/3lab/main.py
@@ -2,22 +2,23 @@ from math import log2, prod
 from argparse import ArgumentParser
 
 argparse = ArgumentParser()
-argparse.add_argument("nums", nargs="+", type=int)
+argparse.add_argument("value", type=int)
+argparse.add_argument("degree", type=int)
+argparse.add_argument("module", type=int)
 
 args = argparse.parse_args()
 
 
 def exponentiation(value: int, degree: int, module: int) -> int:
-    t = int(log2(degree))
-    a = [(value ** (2 ** i)) % module for i in range(t+1)]
-    value_in_bin = list(bin(degree))[2:]
-    x = [v if int(value_in_bin[i]) != 0 else 1 for i, v in enumerate(a[::-1])]
+    t, value_in_bin = int(log2(degree)), list(bin(degree))[2:]
+    a = [(value ** (2**i)) % module for i in range(t, -1, -1)]
+    x = [v for i, v in enumerate(a) if int(value_in_bin[i])]
 
     return prod(x) % module
 
 
 def main():
-    value, degree, module = args.nums
+    value, degree, module = args.value, args.degree, args.module
     print(exponentiation(value, degree, module))
 
 
