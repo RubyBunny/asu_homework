65c6e3d3b35695958a38f706b9f5df78c6ddef14
rubybunny <rubybunny2501@gmail.com>
2023-12-12 22:03:55 +0700

    update (9lab/4.1): Добавлена 9 лабораторная

---
+++ Cryptographic/9lab/4.1/package/util.py
@@ -0,0 +1,18 @@
+def extended_euclidean_algorithm(a: int, b: int) -> tuple[int, int]:
+    x, x_old, y, y_old = 1, 0, 0, 1
+
+    while b:
+        q = a // b
+        a, b = b, a % b
+        x, x_old = x_old, x - x_old * q
+        y, y_old = y_old, y - y_old * q
+
+    return (x, y)
+
+
+def euler_function(p: int, q: int) -> int:
+    return (p - 1) * (q - 1)
+
+
+def convert_bytes2string(byte_list: list[int]) -> str:
+    return "".join(list(map(chr, byte_list)))
